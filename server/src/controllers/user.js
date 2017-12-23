84e6aedf054813d4266d265f269cc34024870657
dibosh <dibosh@newscred.com>
2017-12-23 18:40:53 +0600

    Refactor user controller

--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -13,10 +13,7 @@ controller.createUser = (email, password, firstName, lastName) => {
     user.lastName = lastName;
     bcrypt.hash(password, config.PASSWORD_HASH_SALT).then((hash) => {
       user.passwordHash = hash;
-      user.save((err, savedUser) => {
-        utils.handleErrorIfAny(err, savedUser, reject);
-        resolve(savedUser);
-      });
+      saveUser(user, resolve, reject);
     });
   });
 };
@@ -39,16 +36,31 @@ controller.getUserById = (id) => {
   });
 };
 
-controller.updateUser = (toBeUpdatedUser) => {
+controller.updateUser = (id, updates) => {
   return new Promise((resolve, reject) => {
-    User.findOne({email: toBeUpdatedUser.email}, (err, user) => {
+    User.findById(id, (err, user) => {
       utils.handleErrorIfAny(err, user, reject);
-      user = toBeUpdatedUser;
-      user.save((err, savedUser) => {
-        utils.handleErrorIfAny(err, savedUser, reject);
-        resolve(savedUser);
-      });
+      user.email = updates.email || user.email;
+      user.firstName = updates.firstName || user.firstName;
+      user.lastName = updates.lastName || user.lastName;
+      user.avatarUrl = updates.avatarUrl || user.avatarUrl;
+
+      if (updates.password) {
+        bcrypt.hash(updates.password, config.PASSWORD_HASH_SALT).then((hash) => {
+          user.passwordHash = hash;
+          saveUser(user, resolve, reject);
+        });
+      }
+
+      saveUser(user, resolve, reject);
     });
+  });
+};
+
+let saveUser = (user, resolve, reject) => {
+  user.save((err, savedUser) => {
+    utils.handleErrorIfAny(err, savedUser, reject);
+    resolve(savedUser);
   });
 };
 
