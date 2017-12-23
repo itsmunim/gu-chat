317cbdd22929f060b5aad77f46fe2eb0811c849b
dibosh <dibosh@newscred.com>
2017-12-23 17:56:29 +0600

    Few more utils

--- server/src/common/utils.js
+++ server/src/common/utils.js
@@ -5,41 +5,78 @@ let mongoose = require('mongoose'),
 
 var utils = {};
 
-utils.initializeDB = function () {
+utils.initializeDB = () => {
   if (_.isUndefined(utils.db)) {
     var dbURI = 'mongodb://' + config.MONGO_HOST + ':27017/' + config.MONGO_DB;
     utils.db = mongoose.connect(dbURI, { useMongoClient: true });
 
-    mongoose.connection.on('connected', function () {
+    mongoose.connection.on('connected', () => {
       console.log('Mongoose default connection open to ' + dbURI);
     });
 
-    mongoose.connection.on('error', function (err) {
+    mongoose.connection.on('error', (err) => {
       console.log('Mongoose default connection error: ' + err);
     });
 
-    mongoose.connection.on('disconnected', function () {
+    mongoose.connection.on('disconnected', () => {
       console.log('Mongoose default connection disconnected');
     });
   }
 };
 
-utils.getDB = function () {
+utils.getDB = () => {
   return utils.db;
 };
 
-utils.shutdownDB = function () {
+utils.shutdownDB = () => {
   if (utils.db) {
     mongoose.disconnect();
   }
 };
 
-utils.handleHttpRequestPromise = function (httpPromise, res) {
+utils.getErrorInterpretation = (errorObject) => {
+  if (errorObject.hasOwnProperty('statusCode') && errorObject.hasOwnProperty('error')) {
+    // custom error object
+    return {
+      statusCode: _.get(errorObject, 'statusCode'),
+      resolvedResponse: _.get(errorObject, 'error.error') || _.get(errorObject, 'error')
+    };
+  } else {
+    return {
+      statusCode: 500,
+      resolvedResponse: {
+        message: _.get(errorObject, 'message')
+      }
+    }
+  }
+};
+
+utils.createErrorObject = (status, message) => {
+  return {
+    statusCode: status,
+    error: {
+      status: status,
+      message: message
+    }
+  };
+};
+
+utils.handleErrorIfAny = (err, expectedDBRecord, promiseRejectHandler) => {
+  if (err) {
+    promiseRejectHandler(err);
+  }
+
+  if (!expectedDBRecord) {
+    promiseRejectHandler(new Error('Record not found in database.'));
+  }
+};
+
+utils.handleHttpRequestPromise = (httpPromise, res) => {
   httpPromise
-    .then(function (response) {
+    .then((response) => {
       res.status(response.status).send(response.body);
     })
-    .catch(function (errorResponse) {
+    .catch((errorResponse) => {
       var error = utils.getErrorInterpretation(errorResponse);
       res.status(error.statusCode).send(error.resolvedResponse);
     });
